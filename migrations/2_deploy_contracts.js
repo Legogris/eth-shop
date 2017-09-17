78504b3bfc6160a46cb0135d744363f11005e6a5
Robert Edström <robert.edstrom@gmail.com>
2017-09-17 20:02:19 +0200

    Update migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const Shop = artifacts.require("./Shop.sol");
 
 module.exports = (deployer, network, accounts) => {
-  deployer.deploy(Shop, accounts[0]);
+  deployer.deploy(Shop, accounts[0])
+  .then(() => Shop.deployed())
+  .then(shop => {
+    return shop.approveVendor(accounts[0])
+  })
 };
