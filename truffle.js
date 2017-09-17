7ff6d89fb9eadbe5ae0ebbbe6afa3ac95e1adc86
Robert Edström <robert.edstrom@gmail.com>
2017-09-17 17:55:23 +0200

    Add geth local testnet

--- truffle.js
+++ truffle.js
@@ -6,6 +6,11 @@ module.exports = {
       port: 8545,
       gasPrice: 20000000000,
       network_id: "*" // Match any network id
+    },
+    net42: {
+      host: "localhost",
+      port: 8545,
+      network_id: "42"
     }
   }
 };
